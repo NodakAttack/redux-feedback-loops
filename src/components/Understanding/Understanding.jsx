7ff441fb6d18ad24d3494674c1547336bc45db2a
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-09-20 16:36:55 -0500

    understanding component working, used useRef instead of onChange/useSelector for practice

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Understanding = () => {
-  return (
-    <div>Understanding</div>
-  )
-}
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const understanding = useSelector((store) => store.understanding);
 
-export default Understanding
+  const inputRef = useRef(null);
+
+  const next = () => {
+    dispatch({ type: "PUT_UNDERSTANDING", payload: inputRef.current.value });
+    history.push('/')
+    console.log("understanding is ", understanding);
+  };
+
+  return (
+    <>
+      <h1>How are you understanding today? 1-5</h1>
+      <div>
+        <input type="number" ref={inputRef} />
+        <button onClick={next}>Next</button>
+      </div>
+    </>
+  );
+};
+
+export default Understanding;
